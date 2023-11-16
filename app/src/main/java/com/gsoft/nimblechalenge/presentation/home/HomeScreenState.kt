1662dc09401bcd1c122656cc06ee0d57869bb7e5
DevMcGann <gab.mcganicus@gmail.com>
2023-11-16 00:20:30 -0300

    implemented offline first feature into surveys

--- app/src/main/java/com/gsoft/nimblechalenge/presentation/home/HomeScreenState.kt
+++ app/src/main/java/com/gsoft/nimblechalenge/presentation/home/HomeScreenState.kt
@@ -1,10 +1,12 @@
 package com.gsoft.nimblechalenge.presentation.home
 
 import com.gsoft.nimblechalenge.data.model.SurveyResponse
+import com.gsoft.nimblechalenge.domain.model.Survey
 
 data class HomeScreenState(
     val isLoading: Boolean = false,
     val isError: Boolean = false,
     val errorMessage: String = "",
-    val surveyData : SurveyResponse? = null
+    val surveyData : SurveyResponse? = null,
+    val surveys : List<Survey?> = emptyList()
 )
